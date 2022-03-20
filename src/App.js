04aea1cc513954ff5243f6e62e9ea249f72fd444
Jelson J <jelson20@outlook.com>
2022-03-20 19:46:26 +0000

    commit add maskable

--- src/App.js
+++ src/App.js
@@ -1,26 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import '../src/styles/styles.scss';
+import { useState } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+	const [temp, setTemp] = useState(19);
+	const [tempColor, setTemColor] = useState('cloud');
+
+	// Increse temperature
+	const increseTemp = e => {
+		if (temp === 47) return;
+		const newTemp = temp + 1;
+		if (newTemp >= 30) {
+			setTemColor('sun');
+		}
+		setTemp(newTemp);
+	};
+
+	// Decrese temperature
+	const decreseTemp = () => {
+		if (temp === 0) return;
+		const newTemp = temp - 1;
+		if (newTemp < 20) {
+			setTemColor('rain');
+		}
+		setTemp(temp - 1);
+	};
+
+	return (
+		<>
+			<h1>Temperature Control App</h1>
+			<div className='container'>
+				<div className='temperature_display'>
+					<div className={`temperature ${tempColor}`}>{temp}°C</div>
+				</div>
+				<div className='btn'>
+					<button onClick={() => increseTemp()}>+</button>
+					<button onClick={() => decreseTemp()}>-</button>
+				</div>
+			</div>
+		</>
+	);
 }
-
-export default App;
